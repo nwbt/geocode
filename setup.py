38b287363ae42ad35824c652cc8e590c22aa81f9
Dan Catalano <dev@nwbt.co>
2017-05-12 13:08:13 -0600

    create setup file for easy installation

---
+++ setup.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2017 Dan Catalano <dev@nwbt.co>
+#
+# Distributed under terms of the MIT license.
+
+import os
+from setuptools import setup
+
+setup(
+    name='geocode_csv',
+    version='0.2',
+    description='geocode addresses in csv using google place api',
+    author='Dan Catalano',
+    author_email='dev@nwbt.co',
+    url='https://www.nwbt.co',
+    install_requires=[
+        'googlemaps',
+    ]
+)
